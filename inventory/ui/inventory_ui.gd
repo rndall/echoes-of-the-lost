a0bb381bb694669e8e6bd0a9a021d3f509783df9
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-06-23 08:47:55 +0800

    Add hotbar UI and reorganize inventory slots
    Introduce a dedicated hotbar UI and reorganize inventory slot assets and scenes. Renamed itemslot assets to hotbar_*, added inv_slot and hotbar-specific textures (and import configs), and added inv_hotbar_slot_ui.tscn. Updated inventory_slot_ui.tscn to use AnimatedSprite2D/AtlasTexture and font overrides, and inventory_ui.tscn to split hotbar_slots and inv_slots (instancing the new hotbar scene). inventory_ui.gd now collects hotbar and inventory slots and merges them for updates. Also updated menu_ui to use the hotbar slot scene, adjusted some import UIDs/paths and LFS asset OIDs, and changed the inventory input mapping physical keycode (I -> E).

--- inventory/ui/inventory_ui.gd
+++ inventory/ui/inventory_ui.gd
@@ -1,12 +1,17 @@
 extends Control
 
 @onready var player_inv: Inventory = preload("res://inventory/resources/player_inv.tres")
-@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+@onready var inv_slots: Array = $NinePatchRect/inv_slots.get_children()
+@onready var hotbar_slots: Array = $NinePatchRect/hotbar_slots.get_children()
+
+var slots = []
 
 func _ready() -> void:
 	player_inv.update.connect(update_slots)
 	update_slots()
 	
 func update_slots():
+	slots = hotbar_slots + inv_slots
+	
 	for i in range(min(player_inv.slots.size(), slots.size())):
 			slots[i].update(player_inv.slots[i])
