f1e1e56b8c7ac45d514b616bad14d07ca5dcdfea
rndall <96964503+rndall@users.noreply.github.com>
2026-06-30 09:21:00 +0800

    fix: prevent entities from signalling died twice

--- components/hitbox/hitbox_component.gd
+++ components/hitbox/hitbox_component.gd
@@ -4,29 +4,29 @@ extends Area2D
 @export var attack_damage: float = 1.0
 @export var knockback_force: float
 
-
-func _process(_delta: float) -> void:
-	if not monitoring:
-		return
-	
-	for area in get_overlapping_areas():
-		if area is HurtboxComponent:
-			var hurtbox: HurtboxComponent = area
-			
-			if hurtbox.health_component and hurtbox.health_component.can_take_damage:
-				attack_hurtbox(hurtbox) 
+var _active_hurtboxes: Array[HurtboxComponent] = []
 
 
 func attack_hurtbox(hurtbox: HurtboxComponent) -> void:
 	var attack = Attack.new()
-
 	attack.attack_damage = attack_damage
 	attack.knockback_force = knockback_force
 	attack.attack_position = get_owner().global_position
-	
+
 	hurtbox.damage(attack)
+
+
+func _physics_process(_delta: float) -> void:
+	for hurtbox in _active_hurtboxes:
+		if is_instance_valid(hurtbox):
+			attack_hurtbox(hurtbox)
 
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is HurtboxComponent:
-		attack_hurtbox(area)
+		_active_hurtboxes.append(area)
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if area is HurtboxComponent:
+		_active_hurtboxes.erase(area)
