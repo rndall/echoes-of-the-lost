2d3098312893795ee7e85c82098f88fc62b0a1e9
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-07-01 13:37:38 +0800

    Add stone object and material item
    Implement a stone gathering system with a new Stone object that drops stone material items when destroyed. Add stone assets, create stone inventory item resource, and replace weapon pickups in the outside scene with stone instances. Also reorganize hammer assets with proper naming conventions and fix animation length precision in menu UI.

---
+++ objects/stone/stone.gd
@@ -0,0 +1,23 @@
+class_name Stone
+extends StaticBody2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+@onready var stone_item: InvItem = preload("res://inventory/resources/inventory_items/stone.tres")
+@onready var player_inv: Inventory = preload("res://inventory/resources/player_inv.tres")
+
+func _ready() -> void:
+	if GameManager.get_data_value(name, "dead") == true:
+		queue_free()
+		return
+
+	health_component.died.connect(_on_death)
+
+
+func _on_death() -> void:
+	GameManager.store_data_value(name, "dead", true)
+
+	var stone_count = randi_range(1, 2)
+	player_inv.insert(stone_item, stone_count)
+	queue_free()
