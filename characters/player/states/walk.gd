d5cc34fcdfad50b53383bafa90e8437a30b988e8
rndall <96964503+rndall@users.noreply.github.com>
2026-07-01 08:07:44 +0800

    feat: implement player blocking

--- characters/player/states/walk.gd
+++ characters/player/states/walk.gd
@@ -15,9 +15,13 @@ func physics_update(_delta: float) -> void:
 	var input_direction = Input.get_vector("move_left", "move_right", 
 			"move_up", "move_down")
 	
-	if Input.is_action_just_pressed("action") and GameManager.player_weapon:
-		finished.emit(ACTION)
-		return
+	if GameManager.player_weapon:
+		if Input.is_action_just_pressed("action"):
+			finished.emit(ACTION)
+			return
+		elif Input.is_action_just_pressed("block") and GameManager.player_weapon.target == WeaponItem.Target.ENEMY:
+			finished.emit(BLOCKING)
+			return
 	
 	if input_direction == Vector2.ZERO:
 		finished.emit(IDLE)
@@ -26,10 +30,7 @@ func physics_update(_delta: float) -> void:
 	player.facing_direction = input_direction
 	player.velocity = input_direction * player.speed
 	
-	player.animation_tree.set("parameters/Idle/blend_position", player.facing_direction)
 	player.animation_tree.set("parameters/Walk/blend_position", player.facing_direction)
-	player.animation_tree.set("parameters/Attack/blend_position", player.facing_direction)
-	player.animation_tree.set("parameters/Chop/blend_position", player.facing_direction)
 	
 	player.move_and_slide()
 	
--- classes/attack.gd
+++ classes/attack.gd
@@ -3,3 +3,4 @@ class_name Attack
 var attack_damage: float
 var knockback_force: float
 var attack_position: Vector2
+var attacker: Node
